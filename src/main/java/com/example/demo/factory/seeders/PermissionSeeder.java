9a197a0ae4466b9e8d2f06c9751362b35b90aecd
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2024-01-03 14:35:48 +0100

    refactoring code

--- src/main/java/com/example/demo/factory/seeders/PermissionSeeder.java
+++ src/main/java/com/example/demo/factory/seeders/PermissionSeeder.java
@@ -3,19 +3,16 @@ package com.example.demo.factory.seeders;
 import com.example.demo.entity.Permission;
 import com.example.demo.enums.PermissionEnum;
 import com.example.demo.repository.PermissionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class PermissionSeeder {
     private final PermissionRepository permissionRepository;
-
-    public PermissionSeeder(PermissionRepository permissionRepository) {
-        this.permissionRepository = permissionRepository;
-    }
-
     public void seed() {
         List<Permission> permissions = new ArrayList<>();
         for(PermissionEnum permission: PermissionEnum.values()) {
--- src/main/java/com/example/demo/factory/seeders/RoleSeeder.java
+++ src/main/java/com/example/demo/factory/seeders/RoleSeeder.java
@@ -5,21 +5,17 @@ import com.example.demo.entity.Role;
 import com.example.demo.factory.fakers.RoleFaker;
 import com.example.demo.repository.PermissionRepository;
 import com.example.demo.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class RoleSeeder {
     private final RoleRepository roleRepository;
     private final PermissionRepository permissionRepository;
     private final RoleFaker roleFaker;
-
-    public RoleSeeder(RoleRepository roleRepository, PermissionRepository permissionRepository, RoleFaker roleFaker) {
-        this.roleRepository = roleRepository;
-        this.permissionRepository = permissionRepository;
-        this.roleFaker = roleFaker;
-    }
 
     public void seed() {
         List<Permission> permissions = permissionRepository.findAll();
