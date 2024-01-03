e0209a51adde66b999ef023d140265c949d05083
yassine sahyane <62175613+Yassinesahyane89@users.noreply.github.com>
2024-01-03 10:38:21 +0100

    create && init permission enum

---
+++ src/main/java/com/example/demo/enums/PermissionEnum.java
@@ -0,0 +1,19 @@
+package com.example.demo.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PermissionEnum {
+    ADMIN_READ("admin:read"),
+    ADMIN_WRITE("admin:write"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    USER_READ("user:read"),
+    USER_WRITE("user:write"),
+    USER_CREATE("user:create"),
+    USER_DELETE("user:delete");
+
+    private final String permission;
+}
